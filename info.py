19220ef82edbfdabcf74cfd5f58e64914211e7b8
stfilter <123819448+stfilter@users.noreply.github.com>
2023-04-12 10:01:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -68,8 +68,9 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
     
       # URL Shortener #
 
-URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'omegalinks.in')
-URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '20391324905a2fc8ea0d260e3e6d806413b74cff')
+URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'shorturllink.in')
+URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '9fdbf0c8d396ff61d485dd01851f0edd3052e772
+')
 
      # Auto Delete For Group Message (Self Delete) #
 SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 2000))
