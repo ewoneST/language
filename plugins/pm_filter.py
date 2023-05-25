a3b7a9b109d18a9b368c81ed0d920000d7615b45
ewoneST <130844687+ewoneST@users.noreply.github.com>
2023-05-25 12:31:07 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -114,7 +114,7 @@ async def next_page(bot, query):
         
     btn.insert(0,
         [
-            InlineKeyboardButton(text="⚡ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ⚡", url='https://t.me/st_movie_upadate/1546')
+            InlineKeyboardButton(text="⚡ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ⚡", url='https://t.me/st_movie_upadate/1617')
         ]
     )
 
@@ -273,7 +273,7 @@ async def filter_languages_cb_handler(client: Client, query: CallbackQuery):
     )
     btn.insert(0,
         [
-            InlineKeyboardButton(text="⚡ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ⚡", url='https://t.me/st_movie_upadate/1546')
+            InlineKeyboardButton(text="⚡ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ⚡", url='https://t.me/st_movie_upadate/1617')
         ]
     )
     
@@ -814,7 +814,7 @@ async def auto_filter(client, msg, spoll=False):
     )
     btn.insert(0,
         [
-            InlineKeyboardButton(text="⚡ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ⚡", url='https://t.me/st_movie_upadate/1546')
+            InlineKeyboardButton(text="⚡ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ⚡", url='https://t.me/st_movie_upadate/1617')
         ]
     )
 
