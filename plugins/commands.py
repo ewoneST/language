60906bc277671c6f492d9530073aa0eb230b293a
ewoneST <130844687+ewoneST@users.noreply.github.com>
2023-07-05 19:33:44 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -101,9 +101,8 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_chat_action(enums.ChatAction.TYPING)
-        m=await message.reply_sticker("CAACAgIAAxkBAAEJmcNkpWRZPfAgNnhti8GXcx7pcSfpSwACcQgAAoSUQUlvaAkaprvOcy8E")
-        await asyncio.sleep(1)
-        await m.delete()
+        m=await message.reply_sticker("CAACAgIAAxkBAAEJmcNkpWRZPfAgNnhti8GXcx7pcSfpSwACcQgAAoSUQUlvaAkaprvOcy8E")
+        await asyncio.sleep(2)
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
