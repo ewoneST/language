da92fc5e9ed561d5a73c1f07b046f6c99842da46
ewoneST <130844687+ewoneST@users.noreply.github.com>
2023-07-07 21:13:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -764,7 +764,8 @@ async def auto_filter(client, msg, spoll=False):
     if not spoll:
         message = msg
         stiker = await msg.reply_sticker("CAACAgIAAxkBAAEJnqxkqBzWz1sfRAi70Z2Y7inz6RXztgACcAEAAhAabSIN3A9bRLCgiy8E")
-        await stiker.delete(100)
+        await asyncio.sleep(5)
+        await stiker.delete()
         settings = await get_settings(message.chat.id)
         if message.text.startswith("/"): return  # ignore commands
         if re.findall("((^\/|^,|^!|^\.|^[\U0001F600-\U000E007F]).*)", message.text):
