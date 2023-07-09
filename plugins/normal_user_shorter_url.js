f0ad4ac7575005c73a1f188d2f984b9f632a2ff5
ewoneST <130844687+ewoneST@users.noreply.github.com>
2023-07-09 08:18:57 +0530

    Create normal_user_shorter_url.js

---
+++ plugins/normal_user_shorter_url.js
@@ -0,0 +1,10 @@
+//Normal Users
+
+let filter = require('auto-filter');
+
+filter.load('shorter-url', {
+  type: 'normal_user',
+  url: 
+}).then(() => {
+  // Do something after filter is successfully loaded
+});
