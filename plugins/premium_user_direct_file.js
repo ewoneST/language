6845c86f7292ae2af3c507a1d3a997e688982027
ewoneST <130844687+ewoneST@users.noreply.github.com>
2023-07-09 08:16:54 +0530

    Create premium_user_direct_file.js

---
+++ plugins/premium_user_direct_file.js
@@ -0,0 +1,12 @@
+//Code for Premium Users
+
+//Direct File
+
+let filter = require('auto-filter');
+
+filter.load('direct-file', {
+  type: 'premium_user',
+  url: 
+}).then(() => {
+  // Do something after filter is successfully loaded
+});
